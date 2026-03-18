8f96df5ad89911bdc2f6808a91578f209d340b55
Маркіян <jtcjfi@gmail.com>
2026-03-18 21:54:10 +0200

    fix: move error check to top and implement absolute paths

--- src/task3.js
+++ src/task3.js
@@ -1,15 +1,22 @@
 import fs from 'fs'
-
+import path from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 function replaceFileText(fileRepl, oldText, newText) {
-    fs.readFile(fileRepl, 'utf8', (err, data) => {
-        const result = data.replaceAll(oldText, newText)
-        if(err){
+    const absolutePath = path.join(__dirname, '..', fileRepl);
+    fs.readFile(absolutePath, 'utf8', (err, data) => {
+        if (err) {
             console.log('Помилка: ', err)
+            return;
         }
-        fs.writeFile(fileRepl, result, () => {
+
+        const result = data.replaceAll(oldText, newText)
+
+        fs.writeFile(absolutePath, result, () => {
             console.log('зміст файлу було змінено')
         })
     })
 
 }
-replaceFileText('second-lesson-modules-files/src/replace.txt', 'машина' , 'собака')
+replaceFileText('src/replace.txt', 'машина', 'собака')
