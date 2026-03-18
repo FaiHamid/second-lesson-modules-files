aca9894023b3f8b855fb43f52963c594f3408115
ErikViceCode <erikvicesamp@gmail.com>
2026-03-18 21:40:39 +0200

    countSymbl now counts all symbls without spaces
    replaceText works with filePath

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import fs from 'fs/promises';
 
-async function countSymbl(filename) {
+async function countSymbl(fileName) {
     try{
-        const content = await fs.readFile(filename, 'utf8');
-        console.log(content);
+        const content = await fs.readFile(fileName, 'utf8');
+        const noSpaces = content.replace(/ /g, '');
+        console.log(noSpaces.length + "Кількість символів");
     }catch (err){
         console.log(err + "помилкаа");
     }
@@ -22,16 +23,16 @@ async function logTime(){
 }
 logTime();
 
-async function replaceText(fileName, oldText, newText){
+async function replaceText(filePath, oldText, newText){
     try{
-        let content = await fs.readFile(fileName, 'utf8');
+        let content = await fs.readFile(filePath, 'utf8');
         const regex = new RegExp(oldText,"g");
         content = content.replace(regex, newText);
-        await fs.writeFile(fileName, content);
+        await fs.writeFile(filePath, content);
         console.log(`Замінив ${oldText} na ${newText}`);
     }catch (err){
         console.log(err + "помилкаа");
     }
 }
 
-replaceText("article.txt","123", "55555")
+replaceText("texts/article.txt","123", "55555")
